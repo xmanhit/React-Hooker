57b87c6adcecca50e2a61256d4a552220b88518d
xmanhit <nxmanh102@gmail.com>
2020-04-12 19:26:00 +0700

    Updates

---
+++ precache-manifest.e513b186f31e4b34afabc0978e96c1e8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f919ef43d1c28a60d460610cb511bbe",
+    "url": "/React-Hooker/index.html"
+  },
+  {
+    "revision": "343e2797cf4b7c5643ae",
+    "url": "/React-Hooker/static/css/2.0c90e7c6.chunk.css"
+  },
+  {
+    "revision": "9b0d090fce69b7751e64",
+    "url": "/React-Hooker/static/css/main.9e26689c.chunk.css"
+  },
+  {
+    "revision": "343e2797cf4b7c5643ae",
+    "url": "/React-Hooker/static/js/2.c182eab9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Hooker/static/js/2.c182eab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0d090fce69b7751e64",
+    "url": "/React-Hooker/static/js/main.1f743782.chunk.js"
+  },
+  {
+    "revision": "e1d826d836de4d5df315",
+    "url": "/React-Hooker/static/js/runtime-main.710705ed.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Hooker/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Hooker/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Hooker/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Hooker/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Hooker/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
