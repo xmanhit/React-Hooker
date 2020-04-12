5a24ad398c115ff5611ae419a11230694830a095
xmanhit <nxmanh102@gmail.com>
2020-04-12 16:53:20 +0700

    Search movie v1.0

---
+++ src/components/Review.js
@@ -0,0 +1,76 @@
+import React, { useReducer, useEffect } from "react";
+import Header from "./Header";
+import Detail from "./Detail";
+import "../Detail.css";
+
+const initialState = {
+  loading: true,
+  data: {},
+  errorMessage: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DETAIL_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "DETAIL_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case "DETAIL_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    default:
+      return state;
+  }
+};
+
+const Review = ({ match: { params } }) => {
+  const { id } = params;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const DETAIL_MOVIE_API_URL = `https://www.omdbapi.com/?apikey=4a3b711b&i=${id}`;
+
+  useEffect(() => {
+      fetch(DETAIL_MOVIE_API_URL)
+        .then(response => response.json())
+        .then(jsonResponse => {
+    
+        dispatch({
+          type: "DETAIL_MOVIES_SUCCESS",
+          payload: jsonResponse
+        });
+      });
+  }, [DETAIL_MOVIE_API_URL]);
+  const { data, errorMessage, loading } = state;
+  return (
+    <div className="Detail">
+      <Header text="Movie Review" />
+      <div>
+        {loading && !errorMessage ? (
+          <div className="container">
+            <div className="text-center col-12">
+              <div className="spinner-grow" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            </div>
+          </div>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          <Detail  data={data} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Review;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import Review from './components/Review';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ReactDOM.render((
+  <Router>
+    <Route exact path="/" component={App} />
+    <Route path="/review/:id" component={Review} />
+  </Router>
+), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
+
+serviceWorker.unregister();
