afe7e0a59f2ef53e8b692c35fda69680d153fa35
xmanhit <nxmanh102@gmail.com>
2020-04-12 19:28:08 +0700

    Search movie v1.0

---
+++ src/components/App.js
@@ -0,0 +1,173 @@
+import React, { useReducer, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import Pagination from "react-js-pagination";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
+import "../App.css";
+
+const searchDefault = "Spider Man";
+
+const MOVIE_API_URL = `https://www.omdbapi.com/?apikey=4a3b711b&s=${searchDefault}`;
+
+
+const initialState = {
+  loading: true,
+  data: {},
+  errorMessage: null
+};
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+    case "HANDLE_PAGE_CHANGE":
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    
+    fetch(MOVIE_API_URL)
+        .then(response => response.json())
+        .then(jsonResponse => {
+    
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: {
+            movies: jsonResponse.Search,
+            activePage: 1,
+            totalCount: parseInt(jsonResponse.totalResults),
+            searchValue: searchDefault
+          }
+        });
+    });
+  }, []);
+
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+    fetch(`https://www.omdbapi.com/?apikey=4a3b711b&s=${searchValue}`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if (jsonResponse.Response === "True") {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: {
+            movies: jsonResponse.Search,
+            activePage: 1,
+            totalCount: parseInt(jsonResponse.totalResults),
+            searchValue: searchValue
+          }
+        });
+      } else if(jsonResponse.Error === "Something went wrong.") {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: state.data
+        });
+      } else {
+        dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error
+        });
+      }
+    });
+  };
+
+
+  const { data, errorMessage, loading } = state;
+
+  const handlePageChange = (pageNumber) => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+    
+    fetch(`https://www.omdbapi.com/?apikey=4a3b711b&s=${data.searchValue}&page=${pageNumber}`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if (jsonResponse.Response === "True") {
+        dispatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: {
+            movies: jsonResponse.Search,
+            activePage: parseInt(pageNumber),
+            totalCount: parseInt(jsonResponse.totalResults),
+            searchValue: data.searchValue
+          }
+        });
+      } else {
+        dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.Error
+        });
+      }
+    });
+  }
+  // debugger;
+  return (
+    <div className="App">
+      <Header text="Search Films" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <div className="text-center col-12">
+            <div className="spinner-grow" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          data.movies.map((movie, index) => (
+            <Link key={`${index}-${movie.Title}`} className="movie" to={`/review/${movie.imdbID}`}>
+              <Movie movie={movie} />
+            </Link>
+          ))
+        )}
+      </div>
+      {(!loading && !errorMessage) && (
+        <div align="center">
+          <Pagination
+            activePage={data.activePage}
+            itemsCountPerPage={10}
+            totalItemsCount={data.totalCount || 0}
+            pageRangeDisplayed={5}
+            onChange={handlePageChange}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Detail.js
@@ -0,0 +1,76 @@
+import React from "react";
+import 'font-awesome/css/font-awesome.min.css';
+const DEFAULT_PLACEHOLDER_IMAGE = "https://img.icons8.com/plasticine/400/000/image-file.png";
+
+
+const Detail = ({ data }) => {
+  const poster = (data.Poster === "N/A") ? DEFAULT_PLACEHOLDER_IMAGE : data.Poster;
+  return (
+    <div className="container">
+      <div className="page">
+        <div className="breadcrumbs">
+          <a href="/">Home</a>
+          <i className="fa fa-angle-right mx-2" aria-hidden="true"></i>
+          <span>Movie Review</span>
+          <i className="fa fa-angle-right mx-2" aria-hidden="true"></i>
+          <span>{ data.Title }</span>
+        </div>
+
+        <div className="content">
+          <div className="row">
+            <div className="col-md-4">
+              <figure className="movie-poster"><img src={poster} alt="#"/></figure>
+            </div>
+            <div className="col-md-8">
+              <h2 className="movie-title">{data.Title}</h2>
+              <div className="movie-plot">
+                <p>{ data.Plot }</p>
+              </div>
+              <ul className="movie-meta">
+                <li><strong>Rating:</strong>
+                  {
+                    data.Ratings.map((rating, index) => (
+                      <div key={index} className="rating">
+                        <strong className="Source"><i>{ rating.Source }: </i></strong>
+                        <span className="value">{rating.Value}</span>
+                      </div>
+                    ))
+                  }
+                </li>
+                <li><strong>Runtime:</strong> { data.Runtime }</li>
+                <li><strong>Released:</strong> { data.Released } ({data.Country})</li>
+                <li><strong>Category:</strong> { data.Genre }</li>
+              </ul>
+
+              <ul className="starring">
+                <li><strong>Directors:</strong> { data.Director }</li>
+                <li><strong>Writers:</strong> { data.Writer }</li>
+                <li><strong>Actors:</strong> {data.Actors}</li>
+              </ul>
+            </div>
+          </div>
+          <div className="entry-content">
+            <div className="row">
+              <div className="col-6">
+                <p><strong>Awards:</strong> {data.Awards}</p>
+                <p><strong>Language:</strong> {data.Language}</p>
+                <p><strong>Metascore:</strong> {data.Metascore}</p>
+                <p><strong>imdbRating:</strong> {data.imdbRating}</p>
+                <p><strong>imdbVotes:</strong> {data.imdbVotes}</p>
+              </div>
+              <div className="col-6">
+              <p><strong>imdbID:</strong> {data.imdbID}</p>
+              <p><strong>Type:</strong> {data.Type}</p>
+              <p><strong>DVD:</strong> {data.DVD}</p>
+              <p><strong>BoxOffice:</strong> {data.BoxOffice}</p>
+              <p><strong>Production:</strong> {data.Production}</p>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
