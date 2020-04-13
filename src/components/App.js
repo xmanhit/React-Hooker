29495852899cd0c6e2ec2f085467fbd6bc162cae
xmanhit <nxmanh102@gmail.com>
2020-04-13 14:16:09 +0700

    Search movie v1.0

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,7 @@ const reducer = (state, action) => {
 };
 
 
-const App = () => {
+export const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
@@ -168,4 +168,4 @@ const App = () => {
     </div>
   );
 };
-export default App;
+// export default App;
